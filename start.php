6dd38dbca55d6af4653fe35e2f2e7c0ba5c18fde
Paul Maddern <paul@arcadegeek.co.uk>
2013-10-13 10:38:19 +0100

    Initial updates for Laravel 4...

--- start.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @file
- * Just simply adding the snowstorm js file to a bundle container.
- */
-
-Asset::container('snowstorm')->bundle('snowstorm');
-Asset::container('snowstorm')->add('snowstormjs', 'js/snowstorm-min.js');
